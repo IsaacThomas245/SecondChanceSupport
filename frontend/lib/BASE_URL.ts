a791ffbbb4be363aaa2b8cfcb5c99918abe5af04
Isaac Thomas <thomasisaac245@utexas.edu>
2026-06-12 22:07:11 -0500

    Updated backend url to render url

--- frontend/lib/BASE_URL.ts
+++ frontend/lib/BASE_URL.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://127.0.0.1:5000/api/v1";
+// export const BASE_URL = "http://127.0.0.1:5000/api/v1";
 // export const BASE_URL = "https://cs373-backend.secondchancesupport.me/api/v1";
-// export const BASE_URL = "https://cs373-backend.vercel.app/api/v1/";
+export const BASE_URL = "https://secondchancesupport.onrender.com/api/v1";
