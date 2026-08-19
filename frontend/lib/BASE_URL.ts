0c4ab7d2cf02cc2f5b02da1cae18c48df1731e02
IsaacThomas245 <114020944+IsaacThomas245@users.noreply.github.com>
2026-08-19 02:13:04 -0400

    Update BASE_URL to new deployment endpoint

--- frontend/lib/BASE_URL.ts
+++ frontend/lib/BASE_URL.ts
@@ -1,3 +1,3 @@
 // export const BASE_URL = "http://127.0.0.1:5000/api/v1";
 // export const BASE_URL = "https://cs373-backend.secondchancesupport.me/api/v1";
-export const BASE_URL = "https://secondchancesupport.onrender.com/api/v1";
+export const BASE_URL = "https://secondchancesupport-git-593855760640.us-central1.run.app";
